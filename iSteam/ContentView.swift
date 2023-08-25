d6dd0484d96a05918406f2290de20060d566caf3
Marcus Ziadé <marcus.ziade@ouraring.com>
2023-08-25 18:55:59 +0300

    Initial Commit

---
+++ iSteam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iSteam
+//
+//  Created by Marcus Ziadé on 25.8.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iSteam/iSteamApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSteamApp.swift
+//  iSteam
+//
+//  Created by Marcus Ziadé on 25.8.2023.
+//
+
+import SwiftUI
+
+@main
+struct iSteamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
